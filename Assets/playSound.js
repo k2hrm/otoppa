1c0f3be7e47bd5ab39424bac06423265ea973f58
dentaq <katsu.home@gmail.com>
2014-05-07 21:42:34 +0900

    first commit

---
+++ Assets/playSound.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+var sound : AudioClip;
+function OnMouseUpAsButton() {
+  Debug.Log("pressed!!");
+  audio.PlayOneShot(sound);
+}
